89f86c6933aa4e305265007440f7a19528f61321
ADSR1042 <ADSR1042@gmail.com>
2024-08-20 13:30:02 +0800

    修复版本号 补充域名迁移

--- js/content-script.js
+++ js/content-script.js
@@ -16,8 +16,8 @@ $(document).ready(async function () {
 	let config = await loadConfig();
 	globalConfig = config.config;
 
-	//如果是查老师的根目录 即url为 chalaoshi.buzz 或者 http://chalaoshi-de-s.webvpn.zju.edu.cn:8001/ url需要完全匹配
-	if (window.location.href == 'https://chalaoshi.buzz/' || window.location.href == 'http://chalaoshi-de-s.webvpn.zju.edu.cn:8001/') {
+	//如果是查老师的根目录 即url为 chalaoshi.buzz 或者 http://chalaoshi-buzz-s.webvpn.zju.edu.cn:8001/ url需要完全匹配
+	if (window.location.href == 'https://chalaoshi.buzz/' || window.location.href == 'http://chalaoshi-buzz-s.webvpn.zju.edu.cn:8001/') {
 		//获取当前时间
 		let nowTime = new Date().getTime();
 		// //获取本地存储的数据
